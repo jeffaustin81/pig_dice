ac2e46c3a911452c352f015e48b71a5232d13978
Alex Costa & Jeff Austin <student@epicodus.com>
2015-09-16 12:02:32 -0700

    Finish tests, all tests pass

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,19 @@ function Player(playerName, rollScore, totalScore){
   this.rollScore = rollScore;
   this.totalScore = totalScore;
 }
+
+Player.prototype.diceRoll = function() {
+  var roll = (1 + Math.floor(Math.random() * 6));
+  if(roll === 1){
+    this.rollScore = 0;
+    return this.rollScore;
+  }else{
+  this.rollScore = this.rollScore + roll;
+  }
+  return this.rollScore;
+}
+
+Player.prototype.roundScore = function() {
+  this.totalScore += this.rollScore;
+  this.rollScore = 0;
+}
--- specs/specs.js
+++ specs/specs.js
@@ -5,4 +5,29 @@ describe('Player', function() {
     expect(testPlayer.rollScore).to.equal(24);
     expect(testPlayer.totalScore).to.equal(60);
   });
+
+  it("will add roll score to total score", function() {
+    debugger;
+    var testPlayer = new Player("Alex", 0, 0);
+    testPlayer.diceRoll();
+    testPlayer.diceRoll();
+    testPlayer.diceRoll();
+    testPlayer.roundScore();
+    expect(testPlayer.totalScore).to.be.within(0,18);
+  });
+});
+
+describe('Dice', function() {
+  it("will return a random between 0-6, excluding 1", function(){
+    var testPlayer = new Player("Jeff", 0, 0);
+    expect(testPlayer.diceRoll()).to.be.within(0,6);
+
+  });
+
+  it("will return the value of two rolls of the dice", function(){
+    var testPlayer = new Player("Jeff", 0, 0);
+    testPlayer.diceRoll();
+    testPlayer.diceRoll();
+    expect(testPlayer.rollScore).to.be.within(0,12);
+  });
 });

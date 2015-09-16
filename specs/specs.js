af9d4740d41445e5714cab4a2c93c10d5051e104
Alex Costa & Jeff Austin <student@epicodus.com>
2015-09-16 09:40:33 -0700

    Make first spec PASS

--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,8 @@
 describe('Player', function() {
   it("will get a player name, roll score, total score", function(){
     var testPlayer = new Player("Jeff", 24, 60);
-    expect(testContact.playerName).to.equal("Jeff");
-    expect(testContact.rollScore).to.equal(24);
-    expect(testContact.totalScore).to.equal(60);
+    expect(testPlayer.playerName).to.equal("Jeff");
+    expect(testPlayer.rollScore).to.equal(24);
+    expect(testPlayer.totalScore).to.equal(60);
   });
 });

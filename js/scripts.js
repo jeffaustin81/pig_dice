e179735a6e755efb06c68ae54b8ce095dfa53db0
Jeff Austin <jeffaustin81@gmail.com>
2015-09-17 21:52:21 -0700

    Add animation to player 2 win

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 //Making a player Object
 function Player(playerName, roll, rollScore, totalScore) {
-  this.playerName = playerName;
-  this.roll = roll;
-  this.rollScore = rollScore;
-  this.totalScore = totalScore;
+    this.playerName = playerName;
+    this.roll = roll;
+    this.rollScore = rollScore;
+    this.totalScore = totalScore;
 }
 
 //Dice rolling prototype
 Player.prototype.diceRoll = function() {
-  var roll = (1 + Math.floor(Math.random() * 6));
-  if (roll === 1) {
-    this.rollScore = 0;
+    var roll = (1 + Math.floor(Math.random() * 6));
+    if (roll === 1) {
+        this.rollScore = 0;
+        return this.rollScore;
+    } else {
+        this.rollScore = this.rollScore + roll;
+        this.roll = roll;
+    }
     return this.rollScore;
-  } else {
-    this.rollScore = this.rollScore + roll;
-    this.roll = roll;
-  }
-  return this.rollScore;
 };
 
 //add to total
 Player.prototype.roundScore = function() {
-  this.totalScore += this.rollScore;
-  this.rollScore = 0;
+    this.totalScore += this.rollScore;
+    this.rollScore = 0;
 };
 
 
 $(document).ready(function() {
-  // $("#twoplayer").click(function() {
+    // $("#twoplayer").click(function() {
     var player1 = new Player("Player One", 0, 0, 0);
     var player2 = new Player("Player Two", 0, 0, 0);
 
@@ -45,10 +45,45 @@ $(document).ready(function() {
 
     //player 1
     $("button#player1roll").click(function() {
-      // var roll = ;
-      // Still need to evalute this if statement
-      if (player1.diceRoll() === 0) {
+        // var roll = ;
+        // Still need to evalute this if statement
+        if (player1.diceRoll() === 0) {
+            player1.roundScore();
+            $("div#player1").addClass("lightgrey");
+            $("div#player2").removeClass("lightgrey");
+            // $("#player1roll").off();
+            // $("#player2roll").on();
+            $("button#player1roll").hide();
+            $("button#player1hold").hide();
+            $("button#player2roll").show();
+            $("button#player2hold").show();
+        }
+
+        if (player1.roll !== 0) {
+            var output = "&#x268" + (player1.roll - 1) + "; ";
+            // $("#dice").fadeOut(5).fadeIn();
+            document.getElementById('dice').innerHTML = output;
+        }
+
+        $("#dice").addClass("animated flip").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass('animated flip');
+        });
+
+        $("#roll_score1").fadeOut(300).fadeIn().val(player1.rollScore).text(player1.rollScore);
+
+
+    });
+
+    $("button#player1hold").click(function() {
         player1.roundScore();
+        if (player1.totalScore >= 100) {
+            $("div#player2").hide();
+            $("div#player1").hide();
+            $("div#playeronewins").show();
+            $("#dice").addClass("animated infinite flip");
+        }
+        $("#total_score1").val(player1.totalScore).text(player1.totalScore);
+        $("#roll_score1").val(player1.rollScore).text(player1.rollScore);
         $("div#player1").addClass("lightgrey");
         $("div#player2").removeClass("lightgrey");
         // $("#player1roll").off();
@@ -57,87 +92,54 @@ $(document).ready(function() {
         $("button#player1hold").hide();
         $("button#player2roll").show();
         $("button#player2hold").show();
-      }
-
-      if (player1.roll !== 0) {
-        var output = "&#x268" + (player1.roll - 1) + "; ";
-        // $("#dice").fadeOut(5).fadeIn();
-        document.getElementById('dice').innerHTML = output;
-      }
-
-      $("#dice").addClass("animated flip").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-          $(this).removeClass('animated flip');
-      });
-
-      $("#roll_score1").fadeOut(300).fadeIn().val(player1.rollScore).text(player1.rollScore);
-
-
-    });
-
-    $("button#player1hold").click(function() {
-      player1.roundScore();
-      if (player1.totalScore >= 100) {
-        $("div#player2").hide();
-        $("div#player1").hide();
-        $("div#playeronewins").show();
-        $("#dice").addClass("animated infinite flip");
-      }
-      $("#total_score1").val(player1.totalScore).text(player1.totalScore);
-      $("#roll_score1").val(player1.rollScore).text(player1.rollScore);
-      $("div#player1").addClass("lightgrey");
-      $("div#player2").removeClass("lightgrey");
-      // $("#player1roll").off();
-      // $("#player2roll").on();
-      $("button#player1roll").hide();
-      $("button#player1hold").hide();
-      $("button#player2roll").show();
-      $("button#player2hold").show();
 
     });
 
     $("button#player2roll").click(function() {
-      if (player2.diceRoll() === 0) {
+        if (player2.diceRoll() === 0) {
+            player2.roundScore();
+            $("div#player2").addClass("lightgrey");
+            $("div#player1").removeClass("lightgrey");
+            // $("#player2roll").off();
+            // $("#player1roll").on();
+            $("button#player2roll").hide();
+            $("button#player2hold").hide();
+            $("button#player1roll").show();
+            $("button#player1hold").show();
+        }
+
+        if (player2.roll !== 0) {
+            var output = "&#x268" + (player2.roll - 1) + "; ";
+            $("#dice").fadeOut(5).fadeIn();
+            document.getElementById('dice').innerHTML = output;
+        }
+
+        $("#dice").addClass("animated flip").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass('animated flip');
+        });
+
+        $("#roll_score2").fadeOut(300).fadeIn().val(player2.rollScore).text(player2.rollScore);
+    });
+
+    $("button#player2hold").click(function() {
         player2.roundScore();
-        $("div#player2").addClass("lightgrey");
+        if (player2.totalScore >= 100) {
+            $("div#player2").hide();
+            $("div#player1").hide();
+            $("div#playertwowins").show();
+            $("#dice").addClass("animated infinite flip");
+
+        }
+        $("#total_score2").val(player2.totalScore).text(player2.totalScore);
+        $("#roll_score2").val(player2.rollScore).text(player2.rollScore);
         $("div#player1").removeClass("lightgrey");
+        $("div#player2").addClass("lightgrey");
         // $("#player2roll").off();
         // $("#player1roll").on();
         $("button#player2roll").hide();
         $("button#player2hold").hide();
         $("button#player1roll").show();
         $("button#player1hold").show();
-      }
-
-      if (player2.roll !== 0) {
-        var output = "&#x268" + (player2.roll - 1) + "; ";
-        $("#dice").fadeOut(5).fadeIn();
-        document.getElementById('dice').innerHTML = output;
-      }
-
-      $("#dice").addClass("animated flip").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-          $(this).removeClass('animated flip');
-      });
-
-      $("#roll_score2").fadeOut(300).fadeIn().val(player2.rollScore).text(player2.rollScore);
-    });
-
-    $("button#player2hold").click(function() {
-      player2.roundScore();
-      if (player2.totalScore >= 100) {
-        $("div#player2").hide();
-        $("div#player1").hide();
-        $("div#playertwowins").show();
-      }
-      $("#total_score2").val(player2.totalScore).text(player2.totalScore);
-      $("#roll_score2").val(player2.rollScore).text(player2.rollScore);
-      $("div#player1").removeClass("lightgrey");
-      $("div#player2").addClass("lightgrey");
-      // $("#player2roll").off();
-      // $("#player1roll").on();
-      $("button#player2roll").hide();
-      $("button#player2hold").hide();
-      $("button#player1roll").show();
-      $("button#player1hold").show();
 
 
     });
@@ -145,6 +147,6 @@ $(document).ready(function() {
 
 
 
-  // });
+    // });
 
 });
